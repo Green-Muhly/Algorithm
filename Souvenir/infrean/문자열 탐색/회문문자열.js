5627adb6f50db2901166f306d8720ccccb9c4e5e
souvenir718 <mongkey0@naver.com>
2021-07-24 13:05:09 +0900

    [0724] 인프런 - 회문문자열

--- Souvenir/infrean/문자열 탐색/회문문자열.js
+++ Souvenir/infrean/문자열 탐색/회문문자열.js
@@ -1,59 +1,35 @@
 /*
-    등수구하기
-    N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력합니다.
-
-
-    입력 설명
-    첫 줄에 자연수 N이 주어진다.(1<=N<=50) 
-    두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다. 
-
-    출력 설명
-    최대합을 출력한다.
-
+    회문문자열
+    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
+    문자열이 입력되면 해당 문자열이 회문 문자열이며 'YES',  회문 문자열이 아니면 'NO'를 출력한다.
+    단 회문문자열은 대소문자를 구분하지 않는다.
 */
 
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 rl.on("line", function (line) {
-  rl.close();
+    rl.close();
 }).on("close", function () {
-  let arr = [
-    [5, 3, 7, 2, 3],
-    [3, 7, 1, 6, 1],
-    [7, 2, 5, 3, 4],
-    [4, 3, 6, 4, 1],
-    [8, 7, 3, 5, 2],
-  ];
-  let answer = 0;
+    let str = "goooG".toUpperCase().split("");
+    let answer = true;
+    let answer2 = true;
 
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      let flag = 1;
-      for (let k = 0; k < 4; k++) {
-        let nx = i + dx[k];
-        let ny = j + dy[k];
-        if (
-          nx >= 0 &&
-          nx < 5 &&
-          ny >= 0 &&
-          ny < 5 &&
-          arr[nx][ny] >= arr[i][j]
-        ) {
-          flag = 0;
-          break;
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] === str[str.length - i - 1]) {
+        } else {
+            answer = false;
+            break;
         }
-      }
-      if (flag === 1) answer++;
     }
-  }
 
-  console.log(answer);
-  process.exit();
+    let str2 = "goooG".toUpperCase();
+    if (str2.split("").reverse().join("") !== str2) answer2 = false;
+
+    console.log(answer2 ? "YES" : "NO");
+    process.exit();
 });
