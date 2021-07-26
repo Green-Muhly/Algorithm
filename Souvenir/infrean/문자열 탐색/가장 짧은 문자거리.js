8a3ff200d64e315f7892f3524aed60e45654623e
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-07-27 08:07:13 +0900

    [0727] 인프런 - 가장 짧은 문자거리

--- Souvenir/infrean/문자열 탐색/가장 짧은 문자거리.js
+++ Souvenir/infrean/문자열 탐색/가장 짧은 문자거리.js
@@ -1,12 +1,31 @@
 /*
-    회문문자열
-    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
-    문자열이 입력되면 해당 문자열이 회문 문자열이며 'YES',  회문 문자열이 아니면 'NO'를 출력한다.
-    단 회문문자열은 대소문자를 구분하지 않는다.
+    가장 짧은 문자거리
+    한개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요
 */
 
-const soulution = () => {
-    return "answer";
+const str = "teachermode";
+const s = "e";
+
+const soulution = (str, s) => {
+  let arr = [];
+  let arr2 = [];
+  let count = 1000;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === s) count = 0;
+    else count++;
+    arr.push(count);
+  }
+  count = 1000;
+  for (i = str.length - 1; i >= 0; i--) {
+    if (str[i] === s) {
+      count = 0;
+      arr[i] = count;
+    } else {
+      count++;
+      arr[i] = Math.min(arr[i], count);
+    }
+  }
+  return arr;
 };
 
-console.log(soulution());
+console.log(soulution(str, s));
