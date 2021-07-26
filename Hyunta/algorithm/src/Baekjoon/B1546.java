531d1ded6794cf053139bc65a1fe99f065f64fca
Hyunta <mohai2618@naver.com>
2021-07-26 14:55:37 +0900

    210726 백준 1546번 평균 풀이

---
+++ Hyunta/algorithm/src/Baekjoon/B1546.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        double[] arr = new double[n];
+        double sum = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println((arr[i] / arr[n - 1]) * 100);
+            sum += ((arr[i] / arr[n - 1]) * 100);
+        }
+
+        System.out.println(sum/n);
+
+    }
+}
