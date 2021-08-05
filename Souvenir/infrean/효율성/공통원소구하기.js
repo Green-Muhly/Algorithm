dd4a0c846819562b83d784f242da47e0dc3655d9
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-05 09:04:07 +0900

    [0805] 인프런 - 공통원소구하기

--- Souvenir/infrean/효율성/공통원소구하기.js
+++ Souvenir/infrean/효율성/공통원소구하기.js
@@ -1,12 +1,27 @@
 /*
-    
+    공통원소 구하기
+    A,B 두개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
 */
-const arr = [];
-
-const soulution = (arr) => {
-  let answer = "";
-
+const arr1 = [1, 3, 9, 5, 2];
+const arr2 = [3, 2, 5, 7, 8];
+const soulution = (arr1, arr2) => {
+  // let answer = arr1.filter((x) => arr2.includes(x)).sort((a, b) => a - b);
+  let answer = [];
+  let p1 = 0,
+    p2 = 0;
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) {
+      p1++;
+    } else {
+      p2++;
+    }
+  }
   return answer;
 };
 
-console.log(soulution(arr));
+console.log(soulution(arr1, arr2));
