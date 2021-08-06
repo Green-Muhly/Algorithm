1b334889473c71df202193fa5eb5c9340d596e1f
Hyunta <mohai2618@naver.com>
2021-08-06 13:24:34 +0900

    210806 백준 10250번 ACM 호텔

---
+++ Hyunta/algorithm/src/Baekjoon/B10250.java
@@ -0,0 +1,31 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10250 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int Floor = N % H;
+            int num = N / H;
+            if (Floor == 0) {
+                sb.append((H * 100) + num).append('\n');
+            } else {
+                sb.append((Floor * 100 + num + 1)).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
