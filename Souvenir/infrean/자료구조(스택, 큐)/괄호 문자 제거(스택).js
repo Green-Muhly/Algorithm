d6af0216147b70ce42c668c4aa2cb9c410b8bbe7
souvenir718 <mongkey0@naver.com>
2021-08-14 15:36:23 +0900

    [0814] 인프런 - 괄호 문자 제거

--- Souvenir/infrean/자료구조(스택, 큐)/괄호 문자 제거(스택).js
+++ Souvenir/infrean/자료구조(스택, 큐)/괄호 문자 제거(스택).js
@@ -1,12 +1,22 @@
 /*
-    
+    괄호 문자 제거
+    입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 
+    프로그램을 작성하세요.
 */
-const str = "";
+const str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 
 const soulution = (str) => {
-  let answer = "";
+    let answer = "";
+    let stack = [];
 
-  return answer;
+    for (const x of str) {
+        if (x === ")") {
+            while (stack.pop() !== "(");
+        } else {
+            stack.push(x);
+        }
+    }
+    return stack.join("");
 };
 
 console.log(soulution(str));
