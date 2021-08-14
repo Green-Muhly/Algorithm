882c58fa74cc491cd93b48954ec4bf14f755ba6d
souvenir718 <mongkey0@naver.com>
2021-08-14 12:47:22 +0900

    [0814] 인프런 - 올바른 괄호

--- Souvenir/infrean/자료구조(스택, 큐)/올바른 괄호(스택).js
+++ Souvenir/infrean/자료구조(스택, 큐)/올바른 괄호(스택).js
@@ -1,12 +1,25 @@
 /*
-    
+    올바른 괄호
+    괄호가 입력되면 올바른 괄호이면 YES, 올바르지 않으면 NO를 출력합니다.
 */
-const str = "";
+const str = "()()(()))";
 
 const soulution = (str) => {
-  let answer = "";
+    let answer = "YES";
+    let stack = [];
+    if (!str.length % 2) return "NO";
 
-  return answer;
+    for (const x of str) {
+        if (x === "(") {
+            stack.push(x);
+        } else {
+            if (stack.length === 0) return "NO";
+            stack.pop();
+        }
+    }
+    answer = stack.length === 0 ? "YES" : "NO";
+
+    return answer;
 };
 
 console.log(soulution(str));
