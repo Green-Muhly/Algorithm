c1cd6f9ccb52c50b900a5b523a43798ba6442640
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-20 18:34:01 +0900

    [0820] 인프런 - 선택정렬

--- Souvenir/infrean/정렬과 그리디, 결정알고리즘(이분검색)/선택정렬.js
+++ Souvenir/infrean/정렬과 그리디, 결정알고리즘(이분검색)/선택정렬.js
@@ -1,12 +1,19 @@
 /*
-    
+    선택정렬
+    N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+    정렬하는 방법은 선택정렬입니다.
 */
-const str = "";
+const arr = [13, 5, 11, 7, 23, 15];
 
-const soulution = (str) => {
-  let answer = "";
-
-  return answer;
+const soulution = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+  return arr;
 };
 
-console.log(soulution(str));
+console.log(soulution(arr));
