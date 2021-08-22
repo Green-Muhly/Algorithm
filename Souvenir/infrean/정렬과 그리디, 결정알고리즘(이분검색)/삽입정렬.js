dc0648b6db0b6a8a7bc85c9135614ad45b6a4587
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-22 14:56:10 +0900

    [0822] 인프런 - 삽입정렬

--- Souvenir/infrean/정렬과 그리디, 결정알고리즘(이분검색)/삽입정렬.js
+++ Souvenir/infrean/정렬과 그리디, 결정알고리즘(이분검색)/삽입정렬.js
@@ -1,12 +1,22 @@
 /*
-    
+    삽입 정렬
+    N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+    정렬하는 방법은 삽입정렬입니다.
 */
-const str = "";
+const arr = [11, 7, 5, 6, 10, 9];
 
-const soulution = (str) => {
-  let answer = "";
+const soulution = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let temp = i + 1;
+    for (j = i; j >= 0; j--) {
+      if (arr[temp] < arr[j]) {
+        [arr[temp], arr[j]] = [arr[j], arr[temp]];
+        temp = j;
+      }
+    }
+  }
 
-  return answer;
+  return arr;
 };
 
-console.log(soulution(str));
+console.log(soulution(arr));
