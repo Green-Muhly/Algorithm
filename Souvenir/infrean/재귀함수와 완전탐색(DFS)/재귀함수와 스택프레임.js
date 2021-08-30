b7b2bb57f1bea7a3c76b1df938e2643be2ba1615
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-08-31 05:31:32 +0900

    [0831] 인프런 - 재귀와 스택프레임

--- Souvenir/infrean/재귀함수와 완전탐색(DFS)/재귀함수와 스택프레임.js
+++ Souvenir/infrean/재귀함수와 완전탐색(DFS)/재귀함수와 스택프레임.js
@@ -1,10 +1,19 @@
 /*
-    
+    재귀함수
+    자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요.
 */
-const arr = [];
+const N = 3;
 
-const soulution = (arr) => {
-  return arr;
+const soulution = (N) => {
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  }
+
+  DFS(N);
 };
-
-console.log(soulution(arr));
+soulution(N);
+// console.log();
