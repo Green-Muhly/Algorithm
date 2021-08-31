c2484ca4b4966b1e236eb3fee80f6f04a40b3a04
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-09-01 06:31:08 +0900

    [0901] 인프런 - 이진수 출력

--- Souvenir/infrean/재귀함수와 완전탐색(DFS)/이진수 출력(재귀).js
+++ Souvenir/infrean/재귀함수와 완전탐색(DFS)/이진수 출력(재귀).js
@@ -1,10 +1,21 @@
 /*
-    
+    이진수 출력
+    10진수 N이 2진수로 변환하여 출력하는 프로그램을 작성하세요.
 */
-const arr = [];
+const N = 11;
 
-const soulution = (arr) => {
-  return arr;
+const soulution = (N) => {
+  let answer = "";
+  function DFS(L) {
+    if (L === 0) {
+      return;
+    } else {
+      DFS(parseInt(L / 2));
+      answer += L % 2;
+    }
+  }
+
+  DFS(N);
+  return answer;
 };
-
-console.log(soulution(arr));
+console.log(soulution(N));
