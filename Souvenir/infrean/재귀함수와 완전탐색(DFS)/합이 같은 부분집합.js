bf8995f0d93d827ad14ffb686602c935f99946bf
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-09-06 05:46:03 +0900

    [0906] 인프런 - 합이같은부분집합

--- Souvenir/infrean/재귀함수와 완전탐색(DFS)/합이 같은 부분집합.js
+++ Souvenir/infrean/재귀함수와 완전탐색(DFS)/합이 같은 부분집합.js
@@ -1,10 +1,32 @@
 /*
-    
+    합이 같은 부분집합
+    N개의 원소로 구성된 자연수 집합이 주어지면, 이 집합을 두개의 부분집합으로 나누었을 때,
+    두 부분집합의 원소의 합이 서로 같은 경우가 존재하면 Yes를 출력하고 그렇지 않으면 No를
+    출력하는 프로그램을 작성하세요.
+    둘로 나뉘는 두 부분집합은 서로소 집합이며, 두 부분집합을 합하면 입력으로 주어진
+    원래의 집합이 된다.
 */
-const arr = [];
+const arr = [1, 3, 5, 6, 7, 10];
 
 const soulution = (arr) => {
-  return arr;
+  let answer = "NO";
+  let flag = 0;
+  const total = arr.reduce((a, b) => a + b, 0);
+  const N = arr.length;
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === N) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
 };
 
 console.log(soulution(arr));
