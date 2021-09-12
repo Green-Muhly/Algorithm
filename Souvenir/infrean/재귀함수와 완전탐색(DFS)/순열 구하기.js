9550af1457b0db1eb134d6613d938189300b2e2f
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-09-13 07:22:50 +0900

    [0913] 인프런 - 순열구하기

--- Souvenir/infrean/재귀함수와 완전탐색(DFS)/순열 구하기.js
+++ Souvenir/infrean/재귀함수와 완전탐색(DFS)/순열 구하기.js
@@ -1,10 +1,32 @@
 /*
-    
+    순열 구하기
+    10이하의 N개의 자연수가 주어지면 이 중 M개를 뽑아 일렬로 나열하는 방법을
+    모두 출력합니다.
 */
-const arr = [];
+const arr = [3, 6, 9];
+const M = 2;
 
-const soulution = (arr) => {
-  return arr;
+const soulution = (arr, M) => {
+  let answer = [];
+  let tmp = Array.from({ length: M }, () => 0);
+  const N = arr.length;
+  let check = Array.from({ length: N }, () => 0);
+  function DFS(L) {
+    if (L === M) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 0; i < N; i++) {
+        if (check[i] === 0) {
+          check[i] = 1;
+          tmp[L] = arr[i];
+          DFS(L + 1);
+          check[i] = 0;
+        }
+      }
+    }
+  }
+  DFS(0);
+  return answer;
 };
 
-console.log(soulution(arr));
+console.log(soulution(arr, M));
