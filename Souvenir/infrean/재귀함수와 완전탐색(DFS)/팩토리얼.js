6f880339690a9bfba4a97efa1357b6c76745b832
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-09-14 06:45:26 +0900

    [0914] 인프런 - 팩토리얼

--- Souvenir/infrean/재귀함수와 완전탐색(DFS)/팩토리얼.js
+++ Souvenir/infrean/재귀함수와 완전탐색(DFS)/팩토리얼.js
@@ -1,10 +1,30 @@
 /*
     
 */
-const arr = [];
+const num = 5;
 
-const soulution = (arr) => {
-  return arr;
+const soulution = (num) => {
+  let answer = 0;
+
+  /*
+  내가 푼 풀이
+  function DFS(L, sum) {
+    if (L === 0) {
+      answer = sum;
+      return;
+    } else {
+      DFS(L - 1, sum * L);
+    }
+  }
+  DFS(num, 1);
+  */
+
+  function DFS(n) {
+    if (n === 1) return 1;
+    else return DFS(n - 1) * n;
+  }
+  answer = DFS(n);
+  return answer;
 };
 
-console.log(soulution(arr));
+console.log(soulution(num));
