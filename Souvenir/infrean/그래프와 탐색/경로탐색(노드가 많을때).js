ad0d8ac32ba24e70298ef14cdc4554390b050aba
DESKTOP-FTL6QA2\Souvenir <mongkey0@naver.com>
2021-09-24 07:02:58 +0900

    [0924] 인프런 - 인접리스트

--- Souvenir/infrean/그래프와 탐색/경로탐색(노드가 많을때).js
+++ Souvenir/infrean/그래프와 탐색/경로탐색(노드가 많을때).js
@@ -1,10 +1,53 @@
 /*
-   
+   경로탐색(인접리스트) - 정점 개수가 많을때
+   방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지
+   수를 출력하는 프로그램을 작성하세요.
 */
-const soulution = () => {
-  let answer;
+
+const N = 5;
+const arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+
+const soulution = (N, arr) => {
+  let answer = 0;
+  let graph = Array.from(Array(N + 1), () => Array());
+  let ch = Array.from({ length: N + 1 }, () => 0);
+  let path = [];
+
+  for (let [a, b] of arr) {
+    graph[a].push(b);
+  }
+
+  function DFS(v) {
+    if (v === N) {
+      answer++;
+      console.log(path);
+    } else {
+      for (let i = 0; i < graph[v].length; i++) {
+        if (ch[graph[v][i]] === 0) {
+          ch[graph[v][i]] = 1;
+          path.push(graph[v][i]);
+          DFS(graph[v][i]);
+          ch[graph[v][i]] = 0;
+          path.pop();
+        }
+      }
+    }
+  }
+  path.push(1);
+  ch[1] = 1;
+  DFS(1);
 
   return answer;
 };
 
-console.log(soulution());
+console.log(soulution(N, arr));
