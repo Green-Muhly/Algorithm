d7035a8c9f37d95dbb5e879e1fcf65cd92bbccbd
Hyunta <mohai2618@naver.com>
2021-10-27 20:13:57 +0900

    백준 실버5 캠핑

---
+++ Hyunta/algorithm/src/Baekjoon/B4796.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B4796 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int i = 1;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (l + p + v == 0) break;
+
+            int res = (v / p) * l;
+            res += Math.min((v % p), l);
+            System.out.printf("Case %d: %d %n", i, res);
+            i++;
+        }
+    }
+}
