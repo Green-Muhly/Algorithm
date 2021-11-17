b2141d0017ae96d2d4f63826ba72158176ae2a61
Hyunta <mohai2618@naver.com>
2021-11-17 13:51:44 +0900

    백준 실버4 좋은 단어

---
+++ Hyunta/algorithm/src/Baekjoon/B3986.java
@@ -0,0 +1,31 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B3986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            for (int j = 0; j < word.length(); j++) {
+                char c = word.charAt(j);
+                if (!stack.isEmpty() && c == stack.peek()) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
