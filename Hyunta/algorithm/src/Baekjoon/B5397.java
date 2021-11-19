6d309f5e9bd8ae2e76f7d5a4175343f5c22f8110
Hyunta <mohai2618@naver.com>
2021-11-19 10:29:59 +0900

    백준 실버3 키로거

---
+++ Hyunta/algorithm/src/Baekjoon/B5397.java
@@ -0,0 +1,51 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B5397 {
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            char[] typings = br.readLine().toCharArray();
+            String password = getPassword(typings);
+            System.out.println(password);
+
+        }
+    }
+
+    private static String getPassword(char[] typings) {
+        sb = new StringBuilder();
+        Stack<Character> lt = new Stack<>();
+        Stack<Character> rt = new Stack<>();
+        for (char typing : typings) {
+            if (typing == '<') {
+                if (!lt.isEmpty()) {
+                    rt.push(lt.pop());
+                }
+            } else if (typing == '>') {
+                if (!rt.isEmpty()) {
+                    lt.push(rt.pop());
+                }
+            } else if (typing == '-') {
+                if (!lt.isEmpty()) {
+                    lt.pop();
+                }
+            } else {
+                lt.push(typing);
+            }
+        }
+        for (int j = 0; j < lt.size(); j++) {
+            sb.append(lt.elementAt(j));
+        }
+        for (int j = rt.size() - 1; j >= 0; j--) {
+            sb.append(rt.elementAt(j));
+        }
+        return sb.toString();
+    }
+}
