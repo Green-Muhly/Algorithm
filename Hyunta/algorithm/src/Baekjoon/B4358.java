b5e1fceb01c283b4f59b11e048451414441b8434
Hyunta <mohai2618@naver.com>
2021-11-26 13:31:48 +0900

    백준 실버1 생태학

---
+++ Hyunta/algorithm/src/Baekjoon/B4358.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class B4358 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashMap<String, Integer> trees = new HashMap<>();
+		int cnt = 0;
+		while (true) {
+			String name = br.readLine();
+			if (name == null || name.length() == 0) {
+				break;
+			}
+			trees.put(name, trees.getOrDefault(name, 0) + 1);
+			cnt++;
+		}
+		Object[] keys = trees.keySet().toArray();
+		Arrays.sort(keys);
+
+		StringBuilder sb = new StringBuilder();
+		for (Object key : keys) {
+			String keyString = (String)key;
+			Integer treeCount = trees.get(keyString);
+			float per = (float)(treeCount * 100) / cnt;
+
+			sb.append(keyString + " " + String.format("%.4f", per) + "\n");
+
+		}
+		System.out.println(sb);
+	}
+}
